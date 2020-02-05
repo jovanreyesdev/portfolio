08fc0c20ac74bdc09e5a7c1a5e8128a31a61e2d1
Jovan Rey D. Reyes <jovan@sge.net.au>
2020-02-05 20:19:45 +0800

    added sass sheets & fixed _document hooks issue

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
-import {
+import Document, {
   Html, Head, Main,
   NextScript,
 } from 'next/document';
 
-const Document = () => {
-  Document.getInitialProps = async (ctx) => {
+class DefaultDocument extends Document {
+  static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
-  };
+  }
 
-  return (
-    <Html>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,500,500i,600,600i,700,700i&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,500,500i,600,600i,700,700i&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
 
-export default Document;
+export default DefaultDocument;
